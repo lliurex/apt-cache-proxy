78a269b4aa7aa934767444b527501fe3219b010d
Luis Garcia Gisbert <luisgg@gmail.com>
2026-06-10 18:14:17 +0200

    Added '/api/mirrors' route to query peers available distros

--- utils/peers_routes.py
+++ utils/peers_routes.py
@@ -1,15 +1,14 @@
 from flask import Blueprint, Response, request, jsonify
 from utils.routes  import check_auth
-from services.peers import get_peers_management, add_url_to_peer, del_url_from_peer
+from services.peers import get_all_peers, add_url_to_peer, del_url_from_peer
+from services.mirrors import get_all_mirrors
 
 routes_peers = Blueprint('routes_peers', __name__)
 
 @routes_peers.route('/api/peers', methods=['GET'])
 def api_peers():
-    """Get all mirrors with status for admin panel"""
-    if not check_auth():
-        return Response("Unauthorized", status=401)
-    return jsonify(get_peers_management())
+    """Get peers list"""
+    return jsonify(get_all_peers())
 
 @routes_peers.route('/api/peers/<name>', methods=['PUT'])
 def api_add_peers(name):
@@ -22,7 +21,7 @@ def api_add_peers(name):
     
     if add_url_to_peer(name, urls=urls):
         return jsonify({'status': 'success'})
-    return Response("Failed to add èer URL", status=500)
+    return Response("Failed to add peer URL", status=500)
 
 @routes_peers.route('/api/peers/<name>', methods=['DELETE'])
 def api_del_peers(name):
@@ -36,3 +35,10 @@ def api_del_peers(name):
     if del_url_from_peer(name, urls=urls):
         return jsonify({'status': 'success'})
     return Response("Failed to delete peer URL", status=500)
+
+@routes_peers.route('/api/mirrors', methods=['GET'])
+def api_mirrors():
+    """Get all mirrors with status for admin panel"""
+    return jsonify(get_all_mirrors())
+
+
