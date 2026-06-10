b85ebfd6887f5f53dd813fe33361a3a33eb8aad9
Luis Garcia Gisbert <luisgg@gmail.com>
2026-06-10 19:37:52 +0200

    /api/peers routes updated

--- utils/peers_routes.py
+++ utils/peers_routes.py
@@ -6,39 +6,96 @@ from services.mirrors import get_all_mirrors
 routes_peers = Blueprint('routes_peers', __name__)
 
 @routes_peers.route('/api/peers', methods=['GET'])
-def api_peers():
+def api_peers_get():
     """Get peers list"""
     return jsonify(get_all_peers())
 
-@routes_peers.route('/api/peers/<name>', methods=['PUT'])
-def api_add_peers(name):
-    """Add/update peers URLs"""
+@routes_peers.route('/api/peers', methods=['PUT'])
+def api_peers_put():
+    """Add/Update distro list for a peer"""
     if not check_auth():
         return Response("Unauthorized", status=401)
         
     data = request.get_json()
-    urls = data.get('urls')
-    
-    if add_url_to_peer(name, urls=urls):
-        return jsonify({'status': 'success'})
-    return Response("Failed to add peer URL", status=500)
+    url = data.get('url')
+    distros = data.get('distros')
 
-@routes_peers.route('/api/peers/<name>', methods=['DELETE'])
-def api_del_peers(name):
-    """Delete peers URLs"""
+    if not url or not distros:
+        return Response("Missing url or distros", status=400)
+            
+    if isinstance(distros, str):
+        distros = [distros]
+        
+    if update_distros_by_peer(url, distros):
+        return jsonify({'status': 'success'})
+
+    return Response("Failed to update peer", status=500)
+
+@routes_peers.route('/api/peers', methods=['DELETE'])
+def api_peers_del():
+    """Delete peer from some/all distros"""
+    if not check_auth():
+        return Response("Unauthorized", status=401)
+        
+    data = request.get_json()
+    url = data.get('url')
+    distros = data.get('distros')
+
+    if not url:
+        return Response("Missing url", status=400)
+            
+    if not distros:
+        distros = []
+
+    if isinstance(distros, str):
+        distros = [distros]
+        
+    if update_distros_by_peer(url, distros):
+        return jsonify({'status': 'success'})
+
+    return Response("Failed to delete peer distros", status=500)
+
+@routes_peers.route('/api/peers/<distro>', methods=['GET'])
+def api_peers_distro_get(distro):
+    """Get url peer list for distro"""
+
+    return jsonify(get_all_mirrors()[distro])
+        
+@routes_peers.route('/api/peers/<distro>', methods=['PUT'])
+def api_peers_distro_put(distro):
+    """Add/update url peer list for distro"""
+    if not check_auth():
+        return Response("Unauthorized", status=401)
+
+    urls = data.get('urls')
+
+    if not urls:
+        return Response("Missing urls", status=400)
+
+    if add_url_to_peer(distro, urls=urls):
+        return jsonify({'status': 'success'})
+
+    return Response("Failed to add peer URLs", status=500)    
+        
+@routes_peers.route('/api/peers/<distro>', methods=['DELETE'])
+def api_peers_distro_del(distro):
+    """Delete peers urls for distro"""
     if not check_auth():
         return Response("Unauthorized", status=401)
         
     data = request.get_json()
     urls = data.get('urls')
     
-    if del_url_from_peer(name, urls=urls):
+    if not urls:
+        return Response("Missing urls", status=400)
+
+    if del_url_from_peer(distro, urls=urls):
         return jsonify({'status': 'success'})
+
     return Response("Failed to delete peer URL", status=500)
 
 @routes_peers.route('/api/mirrors', methods=['GET'])
-def api_mirrors():
-    """Get all mirrors with status for admin panel"""
+def api_mirrors_get():
+    """Get all approved mirrors"""
     return jsonify(get_all_mirrors())
-
 
